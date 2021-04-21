f26a6a7d02d0889976cbc8df2f2c906952c8eb3e
Christian Santiago <christiansun9@hotmail.com>
2021-04-21 16:52:58 -0500

    Primeros cambios en la app contador

--- app/src/main/java/com/example/primeraapp/SegundaActivity.kt
+++ app/src/main/java/com/example/primeraapp/SegundaActivity.kt
@@ -1,8 +1,10 @@
 package com.example.primeraapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -10,6 +12,7 @@ class SegundaActivity : AppCompatActivity() {
     var contador = 0
     lateinit var btnIncrementar:Button
     lateinit var btnDecrementar: Button
+    lateinit var btnBack: Button
     lateinit var tvContador:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +23,7 @@ class SegundaActivity : AppCompatActivity() {
         //explicacion git
         btnIncrementar = findViewById(R.id.btnIncrementar)
         btnDecrementar = findViewById(R.id.btnDecrementar)
+        btnBack = findViewById(R.id.btnBack)
         tvContador = findViewById(R.id.tvNumero)
 
 
@@ -38,6 +42,11 @@ class SegundaActivity : AppCompatActivity() {
             tvContador.text = "$contador"
             Log.d("contador","$contador")
         }
+
+        btnBack.setOnClickListener(View.OnClickListener {
+            val miIntent = Intent(this,MainActivity::class.java)
+            startActivity(miIntent)
+        })
 
 
 
--- app/src/main/java/com/example/primeraapp/TercerActivity.kt
+++ app/src/main/java/com/example/primeraapp/TercerActivity.kt
@@ -1,11 +1,30 @@
 package com.example.primeraapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class TercerActivity : AppCompatActivity() {
+    lateinit var imagenUdg: ImageView
+    lateinit var txtNombre: TextView
+    lateinit var txtCarrera: TextView
+    lateinit var txtSemes: TextView
+    lateinit var txtMateria: TextView
+    lateinit var btnBack: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tercer)
+
+        btnBack = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener(View.OnClickListener {
+            val miIntent = Intent(this,MainActivity::class.java)
+            startActivity(miIntent)
+        })
     }
 }
